b41e728a07824c09b78c52f1397e5a4f07468904
Mesqualito <jochen@gebsattel.rocks>
2019-03-21 20:38:41 +0100

    Bad code: perl foreach hash "Odd number of elements in hash assignment at /home/jhassfurter/IdeaProjects/IPTC-Foto-Database/master.pl line 34."

--- master.pl
+++ master.pl
@@ -6,7 +6,7 @@ use Image::ExifTool;
 use Data::Dumper;
 use v5.28;
 
-my @directories = ("/run/user/1000/gvfs/smb-share:server=n4310.local,share=daten/InHouse Produktfotos/Webshop-Bilder/ausgewählt");
+my @directories = ("/home/jhassfurter/Bilder");
 my @suffixes = qw(jpg jpeg gif png raw svg tif tiff psd orf nef eps cr2 arw);
 
 my %file_catalog = ();
@@ -15,7 +15,7 @@ while (my $folder = shift @directories) {
     opendir(DirHandle, "$folder") or die "Cannot open $folder\n";
     my @files = readdir(DirHandle);
     closedir(DirHandle);
-
+    my $i = 1;
     foreach my $file (@files) {
         my $file_string = "$folder/$file";
         if (-f $file_string) {
@@ -27,12 +27,20 @@ while (my $folder = shift @directories) {
                 my %exif_data = ();
                 my $exif_tool = new Image::ExifTool;
                 my $info = $exif_tool->ImageInfo($file_string);
+
+                # read out referenced hash per file-entry with '%{$...}'
                 foreach my $key (keys %{$info}) {
-                    say "Key: $key => Value: %{$info}{$key}";
-                    $exif_data{$key} = %{$info}{$key};
-                 }
-                $file_catalog{$file_string} = %exif_data;
+                    say "Key: $key -> Value: ", $info->{ $key };
+                    %exif_data = $info->{ $key };
+                }
+                # to avoid flattening if read out again
+                # use reference:
+                my $nested_exif_data = \%exif_data;
+                %file_catalog = ($file_string => $nested_exif_data);
+                say "$i. Eintrag für '$file' in '\%file_catalog' geschrieben.";
+                $i++;
             }
+
         }
     }
 }
@@ -41,16 +49,35 @@ say "Im 'file_catalog'-Hash abgelegte Bildpfad(key)-/ Inhalt(value)-Hashes: ", $
 
 # copy one value of our hash into another one
 my $test_entry_key = '/run/user/1000/gvfs/smb-share:server=n4310.local,share=daten/InHouse Produktfotos/Webshop-Bilder/ausgewählt/002375.JPG';
-my %test_entries = ();
-say Dumper(%file_catalog, $test_entry_key);
-@test_entries{ $test_entry_key } = $file_catalog{ $test_entry_key };
+my %test_entries = ($test_entry_key => $file_catalog{ $test_entry_key });
 $hash_size = keys %test_entries;
 say "Im 'test_entries'-Hash abgelegte Bildpfad(key)-/ Inhalt(value)-Hashes: ", $hash_size;
 
 # read out the test-hash
 say "Exif-Daten von '$test_entry_key':";
-say Dumper(%test_entries, $test_entry_key);
+# Dereference the hash from the hash:
+print "$test_entries{$test_entry_key}->{'Title'}";
 
-while (my ($key, $value) = each %{@test_entries{ $test_entry_key }}) {
-    say Dumper($key, $value);
-};
+################## Array reference cheatsheet ##################
+#                                                              #
+#  Array: @names                                               #
+#  create reference: $names_ref = \@names                      #
+#  array-reference: $names_ref                                 #
+#                                                              #
+#                      Array                Array Reference    #
+#  Whole array:        @names               @{ $names_ref }    #
+#  Element of array:   $names[0]            ${ $names_ref }[0] #
+#                                           $names_ref->[0]    #
+#  Size of array:      scalar @names        scalar @$names_ref #
+#  Largest index:      $#names              $#$names_ref       #
+#                                                              #
+################################################################
+
+# use $foo->(args) instead of &$foo(args)
+# use $foo->[subscript] instead of $$foo[subscript]
+# use $foo->{subscript} instead of $$foo{subscript}
+# prefer $foo = value if !defined($foo);
+# over $foo //= value;
+# use if (!cond) {
+# over unless (cond) {
+# use foreach instead of for when looping over a list of elements.
