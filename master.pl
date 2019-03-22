9834fef662ad587293846333986bd30f4c36b40e
Mesqualito <jochen@gebsattel.rocks>
2019-03-22 11:05:06 +0100

    working but again: storing only one hash in the hash %file_catalog

--- master.pl
+++ master.pl
@@ -26,14 +26,10 @@ while (my $folder = shift @directories) {
                 my $exif_tool = new Image::ExifTool;
                 my $info = $exif_tool->ImageInfo($file_string);
                 say "Datei: $file_string";
-                my %dereferenced = ();
+                %file_catalog = ($file_string => $info);
                 foreach (keys %{$info}) {
                     say "Key: $_ => Value: $$info{ $_ }";
-                    %dereferenced = ( $_ => $$info{ $_ });
                 }
-                my $strange_reference = \%dereferenced;
-                %file_catalog = ($file_string => %dereferenced);
-                %file_catalog = ($file_string => $strange_reference);
             }
         }
     }
@@ -41,16 +37,15 @@ while (my $folder = shift @directories) {
 
 my $hash_size = keys %file_catalog;
 print "\n---------------------------\n";
-print "In 'file_catalog'-hash recorded image-path (key)-/ exif-data (value)-Hashes: ", $hash_size;
+print "In 'file_catalog'-hash recorded image-path (key)-/ exif-data (value)-hashes: ", $hash_size;
 print "\n---------------------------\n";
 
-# read out referenced hash per file-entry with '%{$...}'
 my ($i, $j) = 0;
 foreach my $key (sort keys %file_catalog) {
     $i++;
-    say "$i. key: $key";
-    foreach my $value (keys %{$file_catalog{ $key }}) {
+    printf "%s. key: $key\n", uc chr($i + ord('A') - 1 );
+    foreach my $inner_key (keys %{$file_catalog{ $key }}) {
         $j++;
-        say "$j. inner key: $key, subject: $value: $file_catalog{$key}{$value}";
+        say "$j. inner key: $inner_key: $file_catalog{$key}{$inner_key}";
     }
 }
